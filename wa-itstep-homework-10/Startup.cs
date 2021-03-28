58781ffd661d8dc5aeb22c02c6d2b4a11f07c142
NVV <uniondirfolder@gmail.com>
2021-03-28 16:03:48 -0700

    end-85%

--- wa-itstep-homework-10/Startup.cs
+++ wa-itstep-homework-10/Startup.cs
@@ -16,6 +16,7 @@ namespace wa_itstep_homework_10
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,17 @@ namespace wa_itstep_homework_10
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=ConferenceRegistration}/{action=Index}");
+                endpoints.MapRazorPages();
             });
         }
     }
