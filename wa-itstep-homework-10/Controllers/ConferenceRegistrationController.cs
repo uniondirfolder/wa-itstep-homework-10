93ec136baa7a11466df79924df1123808e5d38de
NVV <uniondirfolder@gmail.com>
2021-03-28 13:02:50 -0700

    pit-stop

---
+++ wa-itstep-homework-10/Controllers/ConferenceRegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wa_itstep_homework_10.Models;
+
+namespace wa_itstep_homework_10.Controllers
+{
+    public class ConferenceRegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Time = DateTime.Now;
+            return View("Index");
+        }
+
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public IActionResult Register(Participant participant)
+        {
+            //TODO: Do something with registration info
+            return View();
+        }
+        
+    }
+}
---
+++ wa-itstep-homework-10/Models/Participant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wa_itstep_homework_10.Models
+{
+    public class Participant
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool? Sreaker { get; set; }
+    }
+}
